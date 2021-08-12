ff80751424cfcde2db9a590f7d39e188fa1eff69
Daniel J. McDonald <dajmcdon@gmail.com>
2021-08-12 16:55:20 -0700

    remove old cruft; move to 2021T1

--- Rpackage/.Rprofile
+++
@@ -1,1 +0,0 @@
-Sys.setenv(RETICULATE_PYTHON="/Users/dajmcdon/opt/miniconda3/envs/r-tensorflow/bin/python")
--- lecture-code/06-information-criteria.R
+++
@@ -1,7 +0,0 @@
-## ----setup, include=FALSE, warning=FALSE, message=FALSE----------------------------------
-source("rmd_config.R")
-
-
-## ----------------------------------------------------------------------------------------
-cv_nice <- function(mdl) mean((residuals(mdl)/(1-hatvalues(mdl)))^2)
-
