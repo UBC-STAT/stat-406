c3786581d617422b899f94e4011bc290df1b388b
Daniel J. McDonald <dajmcdon@gmail.com>
2022-08-31 13:41:53 -0700

    update image

--- _rscripts/some-graphics.R
+++ _rscripts/some-graphics.R
@@ -3,23 +3,20 @@ set.seed(20220610)
 x = sort(runif(n, -2*pi, 2*pi))
 fx <- function(x) .75*sin(x) + x/(2*pi)
 y = fx(x) + rnorm(n, sd=.35)
-blue="#00A7E1"
-red="#ff3100"
-orange="#ff8300"
-green="#00af64"
-purple = "#654ea3"
-meadow = "#598234"
-moss = "#aebd38"
-waterfall = "#68829e"
-thunder_cloud = "#505160"
+
+primary <- "#2c365e"
+secondary <- "#e98a15"
+tertiary = "#0a8754"
+fourth_color = "#DBCBD8"
+
 
 library(ggplot2)
 
-ggplot(data.frame(x=x,y=y), aes(x, y)) +
-  geom_point(color=thunder_cloud,alpha=.5, shape = 16) +
+ggplot(data.frame(x = x, y = y), aes(x, y)) +
+  geom_point(color=fourth_color, alpha=.5, shape = 16) +
   theme_void() +
-  stat_function(fun=fx, color=moss, size=2) +
-  geom_smooth(se=FALSE, span=.075, color=meadow) +
+  stat_function(fun=fx, color=secondary, size=2) +
+  geom_smooth(se=FALSE, span=.075, color=primary) +
   #geom_smooth(se=FALSE, span=.15, color=orange) +
-  geom_smooth(se=FALSE, span=.75, color=waterfall)
-ggsave(here::here("assets","img","smooths.jpg"), width=9, height=4.5)
+  geom_smooth(se=FALSE, span=.75, color=tertiary)
+ggsave(here::here("assets", "img", "smooths.jpg"), width = 9, height = 4.5)
